b069b632e46c81e149d694b6cfcf182a8df83e2b
Javaher Ghaedi <129844638+javaherGhaedi@users.noreply.github.com>
2023-11-06 13:25:23 +0330

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accountiing
+{
+    public partial class mainform : Form
+    {
+        public mainform()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CustomerForm i = new CustomerForm();
+            i.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ProductsForm m = new ProductsForm();
+            m.Show();
+        }
+    }
+}
